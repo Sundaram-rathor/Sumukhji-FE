2d1ccc0b171fe89828ec9c5f53c014e6f702cdf9
Sundaram-rathor <121678006+Sundaram-rathor@users.noreply.github.com>
2025-02-13 18:04:04 +0530

    phone updated

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -15,7 +15,7 @@ function ContactUs() {
                 <div className=' text-2xl '>Contact Us</div>
                 <div className='mt-5 text-gray-600'>You need more information? <br /> Check what other persons are saying about our product. <br /> They are very happy with their purchase</div>
                 <div className='mt-10'>
-                    <div className='flex items-center gap-4 mt-4'><WhatsappIcon/> +91 8287733530</div>
+                    <div className='flex items-center gap-4 mt-4'><WhatsappIcon/> +91 7678392908</div>
                     <div className='flex items-center gap-4 mt-4'><MailIcon/> Sumukhship@gmail.com</div>
                     <div className='flex items-center gap-4 mt-4'><PhoneIcon/> +91 8287733530</div>
                     <div className='flex items-center gap-4 mt-4'><InstaIcon/> Instagram</div>
